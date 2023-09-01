0ea8ef4c99a91e0c39b370d27bdb9b3403afc6fe
michho8 <89873670+michho8@users.noreply.github.com>
2023-08-31 20:02:31 -1000

    Create counting.c

---
+++ projects/counting.c
@@ -0,0 +1,20 @@
+---
+layout: project
+type: project
+image: img/abc.jpg
+title: "Counting characters"
+date: April 2022
+published: true
+labels:
+  - C
+  - Commandline
+summary: "Counting the number of occurrences of characters developed in ICS 212."
+---
+
+<img class="img-fluid" src="../img/abc.jpg">
+
+This was an independent assignment that takes some input (whether it be .txt format or manually typed into the command line) and counts the number of occurrences of the letters: 'a', 'b', and 'c'. I had to use the `getchar()` method that will take in the value of the next character from the input. 
+
+Helped to get accustomed to how to use `getchar()` and using the C syntax (for printing out) after having spent two semesters focusing on the Java language.
+
+Source: <a href="https://github.com/michho8/counting-chars"><i class="large github icon "></i>ics212-counting</a>
